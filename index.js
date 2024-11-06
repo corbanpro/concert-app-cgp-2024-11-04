cae33a7bf1f83afb93f27a941ccbd73f1bbb7c49
corbanpro <corbanpro@gmail.com>
2024-11-06 15:37:58 +0000

    fix db info

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ let app = new express();
 const knex = require("knex")({
  client: "mysql",
  connection: {
-  host:"concert-db-instance-1.c61dq6ysma4i.us-east-2.rds.amazonaws.com",
+  host:"concert-db.c1w0g0ag8m9n.us-east-1.rds.amazonaws.com",
   user: "admin",
-  password: "Password1",
+  password: "password123",
   database:"paradise-concerts",
   port: 3306,
  },
